5c3216568e2af92640f75eec7a5332ad967a4f5d
root <root@jenkins.server>
2021-04-03 05:20:20 +0000

    first commit5

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,4 +4,6 @@ echo maven-project > /opt/pipeline/jenkins/deploy/remote-host/.auth
 echo $BUILD_TAG >> /opt/pipeline/jenkins/deploy/remote-host/.auth
 echo $PASS >> /opt/pipeline/jenkins/deploy/remote-host/.auth
 
-docker-compose up -f /opt/pipeline/jenkins/deploy/remote-host.yml
+
+cd jenkins/deploy/ && docker-compose up -d
+
