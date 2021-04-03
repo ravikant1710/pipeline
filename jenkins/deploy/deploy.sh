a6c6329e472238549f402f22ee9507073d43e4f6
root <root@jenkins.server>
2021-04-03 09:12:27 +0000

    first commit12

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-WORKSPACE=/opt/jenkins_home/jenkins_home/workspace/pipeline
-echo maven-project > $WORKSPACE/centos7/auth
-echo $BUILD_TAG >> $WORKSPACE/centos7/auth
-echo $PASS >> $WORKSPACE/centos7/auth
+#WORKSPACE=/opt/jenkins_home/jenkins_home/workspace/pipeline
+echo maven-project > /tmp/.auth
+echo $BUILD_TAG >> /tmp/.auth
+echo $PASS >> /tmp/.auth
 
+scp -r /tmp/.auth root@10.128.0.3:/tmp/
 
-cd jenkins/deploy/ && docker-compose up -d
+scp -r ./jenkins/deploy/docker-compose.yml root@10.128.0.3:/app/maven/
+scp -r ./jenkins/deploy/publish root@10.128.0.3:/app/maven/
 
+ssh root@10.128.0.3 "/app/maven/publish"
