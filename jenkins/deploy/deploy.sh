33a85057a14d4ed644330342a85af891917c0803
root <root@jenkins.server>
2021-04-03 06:29:38 +0000

    first commit7

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,9 +2,9 @@
 
 WORKSPACE=/opt/jenkins_home/jenkins_home/workspace/pipeline
 
-echo maven-project > $WORKSPACE/jenkins/deploy/centos7/.auth
-echo $BUILD_TAG >> $WORKSPACE/jenkins/deploy/centos7/.auth
-echo $PASS >> $WORKSPACE/jenkins/deploy/centos7/.auth
+echo maven-project > $WORKSPACE/jenkins/deploy/centos7/auth
+echo $BUILD_TAG >> $WORKSPACE/jenkins/deploy/centos7/auth
+echo $PASS >> $WORKSPACE/jenkins/deploy/centos7/auth
 
 
 cd jenkins/deploy/ && docker-compose up -d
